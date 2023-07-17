f061158b35876a4a2ae61af6451e869dd1a7e49f
15122002PhamGia <coderhopham@gmail.com>
2023-07-18 00:22:00 +0700

    bt4

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
+<<<<<<< HEAD
 fn main() {
     println!("Hello, Rust Bootcamp by VBI Academy!");
 }
+=======
+fn main() {
+    println!("Hello, Rust Bootcamp by VBI Academy!");
+}
+>>>>>>> da11502 (bt4)
